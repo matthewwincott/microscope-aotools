1014e517514a645ffd8e75447ccf4441e5a32a91
Matthew Wincott <matthew.wincott@eng.ox.ac.uk>
2021-10-12 13:27:56 +0100

    Revise modal control panel

--- microAO/cockpit_device.py
+++ microAO/cockpit_device.py
@@ -36,6 +36,7 @@ import aotools
 import cockpit.devices
 import cockpit.devices.device
 import cockpit.gui.device
+from cockpit.gui.guiUtils import FLOATVALIDATOR
 import cockpit.interfaces.imager
 import numpy as np
 import Pyro4
@@ -52,6 +53,24 @@ from microAO.aoDev import AdaptiveOpticsDevice
 
 
 _ROI_MIN_RADIUS = 8
+
+_DEFAULT_ZERNIKE_MODE_NAMES = {
+    1: "Piston",
+    2: "Tip",
+    3: "Tilt",
+    4: "Defocus",
+    5: "Astig (O)",
+    6: "Astig (V)",
+    7: "Coma (V)",
+    8: "Coma (H)",
+    9: "Trefoil (V)",
+    10: "Trefoil (O)",
+    11: "Spherical",
+    12: "Astig 2 (V)",
+    13: "Astig 2 (O)",
+    14: "Quadrafoil (V)",
+    15: "Quadrafoil (O)",
+}
 
 
 def _np_grey_img_to_wx_image(np_img: np.ndarray) -> wx.Image:
@@ -367,6 +386,18 @@ def log_correction_applied(
         fh.write("Aberrations measured: %s\n" % (sensorless_correct_coef))
         fh.write("Actuator positions applied: %s\n" % (str(actuator_offset)))
 
+class FloatCtrl(wx.TextCtrl):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def value(self):
+        try:
+            val = float(self.GetValue())
+        except Exception as e:
+            val = None
+
+        return val
 
 ModeChangeEvent, EVT_MODE_CHANGED = wx.lib.newevent.NewEvent()
 class _Mode(wx.Panel):
@@ -387,19 +418,29 @@ class _Mode(wx.Panel):
         row_sizer = wx.BoxSizer(wx.HORIZONTAL)
         
         # Label for mode
-        self._label = wx.StaticText(self, label = "z{}".format(self.id+1), size=wx.Size(50,-1))
+        mode_index = self.id+1
+        try:
+            mode_label = _DEFAULT_ZERNIKE_MODE_NAMES[mode_index]
+        except KeyError:
+            mode_label = ""
+
+        self._mode_index = wx.StaticText(self, label=str(mode_index), size=wx.Size(30,-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
+        self._mode_label = wx.StaticText(self, label=mode_label, size=wx.Size(120,-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
 
         # Mode slider: drag to set mode
         default_range = 1.5   # range of slider
         self._slider = wx.Slider(self, value=0, minValue=-100, maxValue=100, size=wx.Size(200,-1))
         self._slider.Bind(wx.EVT_SCROLL, self.OnSlider)
-        self._val_prev = value
 
         # Adjust mode adjustment range. Influences range of slider.
-        self._slider_range = wx.SpinCtrlDouble(self, initial=default_range, inc=0.01, size=wx.Size(140,-1))
+        self._slider_min = FloatCtrl(self, wx.ID_ANY, "0", validator=FLOATVALIDATOR, size=wx.Size(50,-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
+        self._slider_max = FloatCtrl(self, wx.ID_ANY, "1.5", validator=FLOATVALIDATOR, size=wx.Size(50,-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
+        
+        self._slider_min.Bind(wx.EVT_TEXT, self.UpdateValueRanges)
+        self._slider_max.Bind(wx.EVT_TEXT, self.UpdateValueRanges)
 
         # Current mode value
-        self._val = wx.SpinCtrlDouble(self, initial=0, inc=0.001, size=wx.Size(140,-1))
+        self._val = wx.SpinCtrlDouble(self, initial=0, inc=0.001, size=wx.Size(160,-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
         self._val.SetDigits(4)
         self.UpdateValueRanges()
         self._val.SetValue(self.value)
@@ -408,12 +449,14 @@ class _Mode(wx.Panel):
         self._val.Bind(wx.EVT_KILL_FOCUS, self.OnModeLoseFocus)
 
         # Layout
-        row_sizer.Add(self._label, wx.SizerFlags().CentreVertical())
-        row_sizer.Add(self._val, wx.SizerFlags().Expand())
+        row_sizer.Add(self._mode_index, wx.SizerFlags().CentreVertical())
+        row_sizer.Add(self._mode_label, wx.SizerFlags().CentreVertical())
+        row_sizer.Add(self._slider_min, wx.SizerFlags().Expand())
         row_sizer.Add(self._slider, wx.SizerFlags().Expand())
-        row_sizer.Add(self._slider_range, wx.SizerFlags().Expand())
+        row_sizer.Add(self._slider_max, wx.SizerFlags().Expand())
+        row_sizer.Add(self._val, wx.SizerFlags().Expand())
 
-        # Set widget sizerOnModeValue
+        # Set widget sizer
         self.SetSizerAndFit(row_sizer)
 
     def OnSlider(self, evt):
@@ -421,24 +464,25 @@ class _Mode(wx.Panel):
         self.focus = True
 
         # Set value
-        new_val = self._val_prev + self._slider.GetValue()/100 * self._slider_range.GetValue()
-        self.SetValue(new_val)
+        try:
+            val = (self._slider.GetValue()+100)/200 * (self._slider_max.value - self._slider_min.value) + self._slider_min.value
+            self.SetValue(val)
+        except TypeError:
+            pass
 
         # Reset slider and ranges when slide end (mouse released)
         if not wx.GetMouseState().LeftIsDown():
             # Lose focus as released
             self.focus = False
 
-            # Update value ranges and reset slider to middle
-            self._val_prev = self.value
-            self.UpdateValueRanges()
-            self._slider.SetValue(0)
-
     def OnModeValueChange(self, evt):
         new_val = self._val.GetValue()
-        self._val_prev = new_val
         self.UpdateValueRanges(new_val)
         self.SetValue(new_val)
+
+    def OnRangeChange(self, evt):
+        self.UpdateValueRanges()
+        evt.Skip()
     
     def OnModeGetFocus(self, evt):
         self.focus = True
@@ -447,29 +491,39 @@ class _Mode(wx.Panel):
         self.focus = False
 
     def UpdateValueRanges(self, middle=None, range=None):
-        middle = self.GetValue()
-        if range is None:
-            range = self._slider_range.GetValue()
-        if middle is None:
-            middle = self.value
+        min_val =  self._slider_min.value
+        if min_val is not None:
+            self._val.SetMin(min_val)
 
-        self._val.SetMin(middle - range)
-        self._val.SetMax(middle + range)
+        max_val = self._slider_max.value
+        if max_val is not None:
+            self._val.SetMax(max_val)
+
+        self.SetValue(self.value, quiet=True)
 
     def GetValue(self):
         return self._val.GetValue()
 
-    def ChangeValue(self, val):
-        # Change value without emitting
+    def SetValue(self, val, quiet=False):
+        """ Set control value 
+
+            Sets control value. Emits mode change event by default, a quiet flag can be 
+            used to override this behaviour.        
+        """
+        # Set value property
         self.value = val
+
+        # Set value control
         self._val.SetValue(val)
+        
+        # Set slider control value
+        slider_val = 200 * (val - self._slider_min.value)/(self._slider_max.value - self._slider_min.value) - 100 
+        self._slider.SetValue(slider_val)
 
-    def SetValue(self, val):
-        # Set value and emit event
-        self.ChangeValue(val)
-
-        evt = ModeChangeEvent(mode=self.id, value= self.value)
-        wx.PostEvent(self, evt)
+        # Emit mode change event, if required
+        if not quiet:
+            evt = ModeChangeEvent(mode=self.id, value= self.value)
+            wx.PostEvent(self, evt)
 
     @property
     def value(self):
@@ -506,17 +560,25 @@ class _ModesPanel(wx.lib.scrolledpanel.ScrolledPanel):
         heading_panel = wx.Panel(root_panel)
         heading_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        headings = [("Mode", 50), ("Value", 140), ("", 200), ("Range", 140)]
+        headings = [
+            ("", 150), 
+            ("Min", 50), 
+            ("Control", 200), 
+            ("Max", 50), 
+            ("Value", 160)
+        ]
 
         font = wx.Font( wx.FontInfo(10).Bold())
+        flags = wx.SizerFlags().Centre()
+
         for heading in headings:
-            heading_widget = wx.StaticText(heading_panel, label=heading[0], size=wx.Size(heading[1],-1))
+            heading_widget = wx.StaticText(heading_panel, label=heading[0], size=wx.Size(heading[1],-1), style=(wx.ALIGN_CENTRE_HORIZONTAL|wx.ST_NO_AUTORESIZE))
             heading_widget.SetFont(font)
-            heading_sizer.Add(heading_widget)
+            heading_sizer.Add(heading_widget, flags)
 
         heading_panel.SetSizer(heading_sizer)
         
-        root_sizer.Add(heading_panel)
+        root_sizer.Add(heading_panel, flags=wx.SizerFlags().Border(wx.BOTTOM,8))
 
         # Add control per mode
         modes = np.zeros(self._n_modes)
@@ -564,11 +626,11 @@ class _ModesPanel(wx.lib.scrolledpanel.ScrolledPanel):
             self.UpdateModes(modes)
 
     def UpdateModes(self, modes):
-        # Add control per mode
+        # Update each mode
         for i, value in enumerate(modes):
             mode_control = self._mode_controls[i]
             if value != mode_control.value and not mode_control.focus:
-                mode_control.ChangeValue(value)
+                mode_control.SetValue(value, quiet=True)
                 mode_control.UpdateValueRanges()
     
 class MicroscopeAOCompositeDevicePanel(wx.Panel):
